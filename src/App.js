d4e746958eb82622a741017117552be4705e2c97
MUHAMMAD <usman.muhammad@iconpro.com>
2020-10-23 12:06:04 +0200

    Data Visualization using react and Chart.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Bar from './Bar';
+import Doughnut from './Doughnut';
+import Line from './Line';
+
+function App() {
+  return (
+    <div className="App">
+      <Bar />
+      <Doughnut />
+      <Line />
+    </div>
+  );
+}
+
+export default App;
+
+
+
